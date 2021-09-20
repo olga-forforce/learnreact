3b79ac3f9ba99c02738ab72fe38c108363c6dab3
olga-forforce <olga.pererva+qa@forforce.com>
2021-09-20 22:58:01 +0300

    '16.09'

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Clock extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: props.date
+        }
+    }
+
+    componentDidMount() {
+        setInterval(() => this.setState({date: new Date() }), 1000)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h2>Now {this.state.date.toLocaleDateString()}</h2>
+            </div>
+        )
+    }
+}
+
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
+import Clock from "./components/Search/Search";
 
 const App = function () {
     const [isLogin, setIsLogin] = useState(false);
@@ -21,6 +22,8 @@ const App = function () {
 
 
 ReactDOM.render(
-    <React.StrictMode><App/></React.StrictMode>,
+    <React.StrictMode><App/>
+    <Clock date={new Date()} />
+    </React.StrictMode>,
     document.getElementById('root')
 );
