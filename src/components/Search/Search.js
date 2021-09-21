08ce0fc473855b9b415ee85ccbca106b4d13ee00
olga-forforce <olga.pererva+qa@forforce.com>
2021-09-21 19:12:57 +0300

    '21.09'

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,7 +3,14 @@ import './search.css';
 export default function Search() {
   return (
     <div className="form">
-        Form
+        <select className="styled-select">
+            <option></option>
+            <option>one</option>
+            <option>two</option>
+            <option>three</option>
+        </select>
+        <input className="input" type="text" />
+        <input className="input" type="text" />
     </div>
   );
 }
